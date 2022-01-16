ad2c0d7815698ddcdf5d2dde18ebdab0cb98ad1a
chrisbele <55947209+Chrisbelefantis@users.noreply.github.com>
2022-01-16 15:42:55 +0200

    Finished dept overview.

--- routes/user.js
+++ routes/user.js
@@ -13,7 +13,7 @@ const connection = mysql.createConnection({
 
 router.get('/dept',(req,res)=>{
 
-    connection.query(`SELECT * FROM dept WHERE user='${req.userData.email}'`,
+    connection.query(`SELECT * FROM dept INNER JOIN expenses on dept.expense = expenses.expense_id WHERE user='${req.userData.email}'`,
 
     (err1,results) =>{
         if(err1){
@@ -28,15 +28,31 @@ router.get('/dept',(req,res)=>{
         else{
             console.log(results);
 
-            sum = 0
+            //Data preprocessimg
+            let sum = 0
+            let owners_sum = 0
+            let tenants_sum = 0
             for(let i=0; i<results.length; i++){
-                sum+= results[i].amount
+                sum+= results[i].dept_amount
+
+                if(results[i].is_owner_expense){
+                    owners_sum+=results[i].dept_amount
+                }
+                else{
+                    tenants_sum+=results[i].dept_amount
+                }
+
+                let date_splitted = results[i].date.toString().split(/[- :]/);
+                results[i].date = date_splitted[0] + ' ' + date_splitted[2] + ' ' + date_splitted[1] + ' ' + date_splitted[3]
+
             }
 
             res.render('user/dept',{
                 userData: req.userData,
                 dept_records: results,
-                sum: sum
+                sum: sum,
+                tenants_sum: tenants_sum,
+                owners_sum: owners_sum
             })
         }
     });
@@ -60,21 +76,20 @@ router.get('/expenses-addition',(req,res)=>{
 router.post('/expenses-addition/add',(req,res)=>{
 
     //Sanitization is needed here
-    console.log(req.body.expenseType)
     let amount = req.body.amount
     let is_owner_expense = req.body.expenseType === 'ownersExpense' ? 1 : 0
     let appartment_building_id = req.userData.appartmentBuilding
     let description = req.body.expenseCategory
 
     connection.query(
-        `INSERT INTO expenses (expense_id, amount, description, date, is_owner_expense, appartment_building) VALUES (NULL, '${amount}','${description}', NOW(), b'${is_owner_expense}', '${appartment_building_id}'); `,
+        `INSERT INTO expenses (expense_id, expense_amount, description, date, is_owner_expense, appartment_building) VALUES (NULL, '${amount}','${description}', NOW(), b'${is_owner_expense}', '${appartment_building_id}'); `,
         (err1, results)=>{
             if(err1){
                 console.log(err1);
                 res.render('user/expenses_addition',{userData: req.userData,message: 'Σφάλμα κάτα την προσθήκη εξόδου: '+err1.sqlMessage})
             }
             else{
-
+                let expenseId = results.insertId;
                 //Add dept to all owners
                 if(is_owner_expense){
 
@@ -101,10 +116,10 @@ router.post('/expenses-addition/add',(req,res)=>{
                             let values = []
                             let per_user_amount = amount/results.length
                             for(let i=0; i< results.length; i++){
-                                values.push([null,per_user_amount,description,results[i].email])
+                                values.push([null,per_user_amount,description,results[i].email,expenseId])
                             }
 
-                            var sql = "INSERT INTO dept (dept_id, amount, description, user) VALUES ?";
+                            var sql = "INSERT INTO dept (dept_id, dept_amount, description, user, expense) VALUES ?";
 
                             connection.query(sql, [values], (err3,results)=> {
                                 if(err3){
@@ -146,10 +161,10 @@ router.post('/expenses-addition/add',(req,res)=>{
                             let values = []
                             let per_user_amount = amount/results.length
                             for(let i=0; i< results.length; i++){
-                                values.push([null,per_user_amount,description,results[i].email])
+                                values.push([null,per_user_amount,description,results[i].email,expenseId])
                             }
 
-                            var sql = "INSERT INTO dept (dept_id, amount, description, user) VALUES ?";
+                            var sql = "INSERT INTO dept (dept_id, dept_amount, description, user, expense) VALUES ?";
 
                             connection.query(sql, [values], (err3,results)=> {
                                 if(err3){
